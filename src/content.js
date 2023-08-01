2dd62d245b875c69cba81e9c75385668426c2c4b
Matthew Cruz <mcruz@umich.edu>
2023-08-01 12:20:45 -0400

    content.js: test commit by Matthew

--- src/content.js
+++ src/content.js
@@ -28,7 +28,7 @@ export default {
   // },
   main: [
     {
-      title: "Elizabeth Asteriou",
+      title: "Elizabeth Asteriou!",
       text: `Monica Dus, PhD, is Assistant Professor of Molecular, Cellular, and Developmental Biology at the University of Michigan.`,
       video: "https://www.youtube.com/watch?v=jIiyGsNSmlM"
     },
